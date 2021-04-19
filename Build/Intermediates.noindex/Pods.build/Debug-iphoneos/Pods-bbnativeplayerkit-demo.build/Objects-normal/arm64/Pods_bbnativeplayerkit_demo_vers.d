cfc7b538fa2908471fa8c4c744c7ec99874f7dea
Olaf Timme <octimme@gmail.com>
2021-04-19 09:51:02 +0200

    Deleted Build dir

--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-bbnativeplayerkit-demo.build/Objects-normal/arm64/Pods_bbnativeplayerkit_demo_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Olaf/Documents/Projects/BlueBillywig/bbnativeplayerkit-demo/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-bbnativeplayerkit-demo.build/DerivedSources/Pods_bbnativeplayerkit_demo_vers.c
